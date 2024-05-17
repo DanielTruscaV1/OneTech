7db267426ece03a36af7669d9d7eda0dab71ffbc
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-05-17 14:16:02 +0300

    Created the Landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import SignUp from "./components/SignUp"
 import SignIn from "./components/SignIn"
 import Home from "./components/Home"
+import Landing from "./components/Landing"
+
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Landing/>
     </div>
   )
 }
---
+++ src/components/Landing.tsx
@@ -0,0 +1,19 @@
+const Landing = () => {
+  return (
+    <div 
+      id="landing-container"
+    >
+      <h1 id="landing-title">
+        OneTech
+      </h1>
+      <button id="landing-menu">
+        <img src="/landing4.png"/>
+      </button>
+      <h1 id="landing-motto">
+        The ever-evolving online tech community!
+      </h1>
+    </div>
+  )
+}
+
+export default Landing
