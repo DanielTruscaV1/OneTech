7a96e40d15f0af85d7ca6eda6bc1594c9529a3f9
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-05-19 18:03:29 +0300

    Added a css animation.

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,12 +1,56 @@
+import { useState } from "react";
 import styles from "./LeftSidebarStyle.module.css"
 
 import { useNavigate } from "react-router-dom"
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
+  const [isExtended, setIsExtended] = useState(true);
+
+  const addAnimation = () => {
+    const container = document.getElementById("left_sidebar_container");
+    const toggle = document.getElementById("left_sidebar_toggle");
+  
+    if (isExtended) {
+      container?.classList.remove("slide-right");
+      toggle?.classList.remove("rotate-180-cw");
+  
+      if(toggle) refreshElement(toggle);
+
+      // Remove and re-add the class to re-trigger the animation
+      setTimeout(() => {
+        container?.classList.add("slide-left");
+        toggle?.classList.add("rotate-180-cw");
+      }, 10);
+    } else {
+      container?.classList.remove("slide-left");
+      toggle?.classList.remove("rotate-180-cw");
+  
+      if(toggle) refreshElement(toggle);
+
+      // Remove and re-add the class to re-trigger the animation
+      setTimeout(() => {
+        container?.classList.add("slide-right");
+        toggle?.classList.add("rotate-180-cw");
+      }, 10);
+    }
+  
+    setIsExtended(!isExtended);
+  };
+
+  const refreshElement = (element: HTMLElement) => {
+    const parent = element.parentNode;
+    const nextSibling = element.nextSibling;
+    if (parent) {
+      parent.removeChild(element);
+      setTimeout(() => {
+        parent.insertBefore(element, nextSibling);
+      }, 10); // Timeout ensures that the element is re-added after a brief delay
+    }
+  };
 
   return (
-    <main className={styles.container}>
+    <main id="left_sidebar_container" className={styles.container}>
       <button 
         className={styles.button}
         onClick={() => {navigate('/home')}}
@@ -60,7 +104,11 @@ const LeftSidebar = () => {
         <img src="/left_sidebar_6.png"/>
         Sign-Up
       </button>
-      <button className={styles.toggle}>
+      <button 
+        id="left_sidebar_toggle"
+        className={styles.toggle}
+        onClick={addAnimation}
+      >
         <img src="/left_sidebar_7.png"/>
       </button>
     </main>
