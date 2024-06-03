103843594827591ecdbb373c725e469c555628ec
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-06-03 15:59:43 +0300

    Small update 2.

--- src/new_components/Profile.tsx
+++ src/new_components/Profile.tsx
@@ -35,7 +35,7 @@ const Profile = () => {
 
     const handleFollow = async () => {
 
-        if(user && (user.followedUsers.length == 0 || !user.followedUsers.includes("456")))
+        if(user && (!user.followedUsers || !user.followedUsers.includes("456")))
         {
             const response = await axios.get(
                 "https://onetech.onrender.com/api/emails"
