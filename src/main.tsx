c3163847af96c6f19827a9719b07929a407042f8
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-07-16 20:59:46 +0300

    Updated the landing page.

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,10 @@ import {
 } from "react-router-dom";
 
 //Import the components
-import App from './App';
 import SignUp from "./components/SignUp"
 import SignIn from "./components/SignIn"
 import Home from "./new_components/Home"
-import Landing from "./components/Landing"
+import Landing from "./new_components/Landing"
 import UserProfile from './components/UserProfile';
 
 //Import the CSS file
@@ -38,8 +37,8 @@ const router = createBrowserRouter([
     )
   },
   {
-    path: "/app",
-    element: <App/>
+    path: "/",
+    element: <Landing/>
   },
   {
     path: "/landing",
---
+++ src/new_components/Landing.tsx
@@ -0,0 +1,61 @@
+import styles from "./LandingStyle.module.css";
+
+const Landing = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.square1}>
+        
+        </div>
+        <div className={styles.square2}>
+        
+        </div>
+        <div className={styles.square3}>
+          
+        </div>
+
+        <div className={styles.square4}>
+        
+        </div>
+        <div className={styles.square5}>
+        
+        </div>
+        <div className={styles.square6}>
+          
+        </div>
+
+        <h1 className={styles.title}>
+            One 
+            <span style={{
+                color:"#3ECFC9",
+                fontWeight: "500",
+            }}>
+            Tech</span>
+        </h1>
+        <br/>
+        <p>
+            The number one online educational platform for computer science and artificial inteligence.
+        </p>
+        <div className={styles.button_container}>
+            <button>
+                Sign-up for free
+            </button>
+            <p>
+                or
+            </p>
+            <button>
+                Sign-in to your account
+            </button>
+        </div>
+        <img 
+            src="/new_landing1.png"
+            className={styles.image1}
+        />
+        <img 
+            src="/new_landing2.png"
+            className={styles.image2}
+        />
+    </div>
+  )
+}
+
+export default Landing
