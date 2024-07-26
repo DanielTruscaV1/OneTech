ebbd1113de605e4e648b6398540ccd3ec4ea9c81
DanielTruscaV1 <danieltrusca2008@gmail.com>
2024-07-26 15:18:52 +0300

    Updated the Theory page.

--- src/new_components/Theory.tsx
+++ src/new_components/Theory.tsx
@@ -1,92 +1,101 @@
 import styles from "../new_styles/TheoryStyle.module.css"
 import Sidebar from "./Sidebar"
-import Top from "./Top"
-
-import { getUserData, User } from '../getUser.tsx';
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const Theory = () => {
-    const navigate = useNavigate();
-
-    const [user, setUser] = useState<User | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);  
-
-    const user_id = localStorage.getItem("userID");
-
-    const [articles, setArticles] = useState<any>([]);
-
-    useEffect(() => {
-        const fetchUserInfo = async () => {
-          const userData = await getUserData(user_id);
-    
-          if (userData) {
-            setUser(userData);
-          } else {
-            setError('Failed to fetch user data');
-          }
-          setLoading(false);
-        };
-    
-        const fetchArticles = async () => {
-          try 
-          {
-              const response = await axios.get(
-                "https://onetech.onrender.com/api/getArticles"
-              );
-
-              setArticles(response.data.result.data)
-          }
-          catch(error)
-          {
-              console.log(error);
-          }
-        }
-
-        fetchUserInfo();
-
-        fetchArticles();
-      }, []);
-
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
 
   return (
     <div className={styles.theory}>
         <Sidebar/>
-        <Top user={user}/>
-        <div className={styles.content}>          
-            {
-              articles.map((a: any) => {
-                return <div 
-                    className={styles.article}
-                    onClick={() => navigate(`/article/${a.data.article_id}`)}
-                  >
-                  <img src={a.data.thumbnail}/>
-                  <div className={styles.article_info}>
-                    <h1>
-                      {a.data.title}
-                    </h1>
-                    <div style={{display: "inline-block"}}>
-                      <div style={{width: "40vw", height: "20px", backgroundColor: "rgb(245, 245, 245)"}}>
-                      </div>
-                      <div style={{marginTop: "-20px", width: "20vw", height: "20px", backgroundColor: "#00ADB5"}}>
-                      </div>
-                    </div>
-                    <h1 style={{marginLeft: "1.5vw", display: "inline-block"}}>
-                      50%
-                    </h1>
-                    <br/>
-                    <p>
-                      | {a.data.views} views | {a.data.likes} likes | {a.data.read} |
-                    </p>
-                  </div>
-                </div>
-              })
-            }
+        <div className={styles.problemset}>
+      <Sidebar />
+      <div className={styles.row1}>
+        <h1>
+          OneTech / Theory
+        </h1>
+      </div>
+      <div className={styles.row2}>
+        <p>
+          Roadmaps
+        </p>
+        <button>
+          Search
+        </button>
+        <button>
+          Filter
+        </button>
+      </div>
+      <div className={styles.row23}>
+        <div className={styles.box}>
+          <h2>
+              Computer Science (CS)
+            </h2>
+            <button>
+              View roadmap
+            </button>
         </div>
+        <div className={styles.box}>
+          <h2>
+              Artificial Intelligence (AI)
+            </h2>
+            <button>
+              View roadmap
+            </button>
+        </div>
+      </div>
+      <div className={styles.row3}>
+        <p>
+          Tutorials 
+        </p>
+        <button>
+          Search
+        </button>
+        <button>
+          Filter
+        </button>
+      </div>
+      <div className={styles.row34}>
+        <div className={styles.box}>
+          <h2>
+              JavaScript
+          </h2>
+          <p>
+            5 hours of content
+          </p>
+          <p>
+            Experience level - Beginner
+          </p>
+          <p>
+            32 chapters
+          </p>
+          <p>
+            Community support avaiable
+          </p>
+          <button>
+            Get started
+          </button>
+        </div>
+        <div className={styles.box}>
+          <h2>
+              TypeScript
+          </h2>
+          <p>
+            5 hours of content
+          </p>
+          <p>
+            Experience level - Beginner
+          </p>
+          <p>
+            32 chapters
+          </p>
+          <p>
+            Community support avaiable
+          </p>
+          <button>
+            Get started
+          </button>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
